edb24522b67fc31293a4819cdf1ed777fbe829f2
tndhaba-creator <tndhaba@gmail.com>
2025-08-16 23:55:50 -0400

    Delete api directory

--- api/internal/schema.js
+++
@@ -1,9 +0,0 @@
-import { logRequest } from '../_utils/log.js';
-export default async function handler(req, res) {
-  const start = Date.now();
-  try {
-    res.status(200).json({ ok:true, internal:true, version:'v2-beta', note:'not linked' });
-  } finally {
-    logRequest(req, res, start, { kind:'DECOY' });
-  }
-}
--- api/tools/list.js
+++
@@ -1,9 +0,0 @@
-import { logRequest } from '../_utils/log.js';
-export default async function handler(req, res) {
-  const start = Date.now();
-  try {
-    res.status(200).json({ tools:['products','search','checkout','status'] });
-  } finally {
-    logRequest(req, res, start, { kind:'DECOY' });
-  }
-}
