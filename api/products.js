4f4f09b9457c5aa602eef59d1b60ee2d6563b97a
tndhaba-creator <tndhaba@gmail.com>
2025-08-18 14:31:47 -0400

    Delete api directory

--- api/products.js
+++
@@ -1,8 +0,0 @@
-
-export default function handler(req, res) {
-  res.status(200).json([
-    {id:101, name:"Widget Pro", price:175, warranty:"2 years", connector:"USB-C"},
-    {id:202, name:"Gizmo X", price:169, warranty:"1 year", connector:"USB-C"},
-    {id:303, name:"Thingamabob", price:150, warranty:"6 months", connector:"Micro-USB"}
-  ]);
-}
